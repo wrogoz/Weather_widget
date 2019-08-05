fa10e9f4e4569a9b2b92460295da4e60e4b453b4
Wojciech Rogóż <wrogoz@gmail.com>
2019-08-05 13:24:08 +0200

    added menu to city Name

--- src/components/weather/weather.tsx
+++ src/components/weather/weather.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-
+import {SimpleMenu,MenuItem} from '@rmwc/menu';
 import {DaysList} from './daysList';
 import {DateChoiceComp} from './dateChoiceComp';
 
@@ -9,7 +9,13 @@ export class Weather extends React.Component<{},{}>{
         return(
             <WeatherContainer>
                 <TopBox>
-                    <CityName>New York, NY</CityName>
+                    
+                        <SimpleMenu handle={<CityName>New York, NY</CityName>}>
+                        <MenuItem>Katowice</MenuItem>
+                        <MenuItem>Sosnowiec</MenuItem>
+                        <MenuItem>Londyn</MenuItem>
+                        </SimpleMenu>
+
                     <Date>Tuesday April 15th</Date>
                     <Date>Overcast</Date>
                 </TopBox>
@@ -34,6 +40,7 @@ const TopBox = styled.div`
 `
 const CityName = styled.div`
     font-size:36px;
+    cursor: pointer;
 
 `
 const Date = styled.div`
