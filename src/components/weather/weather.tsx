8bb832a17dc6455cc36801ad6ce80463e6884497
Wojciech Rogóż <wrogoz@gmail.com>
2019-08-09 08:31:21 +0200

    exporting getDataForCity to store

--- src/components/weather/weather.tsx
+++ src/components/weather/weather.tsx
@@ -6,7 +6,7 @@ import {DateChoiceComp} from './weatherComponents/dateChoiceComp';
 import store,{Store} from '../../store/store';
 import {observer} from 'mobx-react';
 import axios from 'axios';
-import  moment from 'moment';
+
 
 
 
@@ -17,48 +17,16 @@ interface WeatherProps {
 
 @observer
 export class Weather extends React.Component<WeatherProps,{}>{
-
+   
+   
+    
    
 
     selectCity(event:any) {
         
         this.props.store.city=event;
         
-        for (let i=0; i<this.props.store.cityNames.length; i++){
-
-            if(this.props.store.cityNames[i].name.indexOf(event)!==-1){
-                
-                axios.get(`https://dev-weather-api.azurewebsites.net/api/city/${this.props.store.cityNames[i].id}/weather?date=${this.props.store.selectedDate}`)
-                .then( (res:any) => {
-
-                    this.props.store.api=(res.data);
-
-                    this.props.store.days=[];
-                    this.props.store.temperature=[];
-                    this.props.store.pollenCount=[];
-                    this.props.store.speed=	[];
-                    this.props.store.direction=	[];
-                   
-                    for(let i =0; i<this.props.store.api.length; i++){
-                        this.props.store.days.push(moment(this.props.store.api[i].date).format("dddd"));
-                        this.props.store.temperature.push(this.props.store.api[i].temperature);
-                        this.props.store.pollenCount.push(this.props.store.api[i].pollenCount);
-                        this.props.store.speed.push(this.props.store.api[i].windInfo.speed);
-                        this.props.store.direction.push(this.props.store.api[i].windInfo.direction);
-                    }
-               
-                  
-                    
-
-
-
-                
-                })
-                .catch( (error) => {
-                console.log(error);
-                })
-            }
-        }
+       this.props.store.getDataForCity();
     }
 
     componentDidMount(){
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,6 @@
 import {observable} from 'mobx';
+import axios from 'axios';
+import moment from 'moment';
 
 export class Store {
     @observable api:any=[''];
@@ -19,6 +21,36 @@ export class Store {
   
     @observable type:string[]| null=["RainLight"];
     //-------------------------------------------------------------------------/
+    getDataForCity = ()=>{
+        for (let i=0; i<this.cityNames.length; i++){
+
+            if(this.cityNames[i].name.indexOf(this.city)!==-1){
+                
+                axios.get(`https://dev-weather-api.azurewebsites.net/api/city/${this.cityNames[i].id}/weather?date=${this.selectedDate}`)
+                .then( (res:any) => {
+
+                    this.api=(res.data);
+
+                    this.days=[];
+                    this.temperature=[];
+                    this.pollenCount=[];
+                    this.speed=	[];
+                    this.direction=	[];
+                   
+                    for(let i =0; i<this.api.length; i++){
+                        this.days.push(moment(this.api[i].date).format("dddd"));
+                        this.temperature.push(this.api[i].temperature);
+                        this.pollenCount.push(this.api[i].pollenCount);
+                        this.speed.push(this.api[i].windInfo.speed);
+                        this.direction.push(this.api[i].windInfo.direction);
+                    }
+                })
+                .catch( (error) => {
+                console.log(error);
+                })
+            }
+        }
+    }
 }
 
 
